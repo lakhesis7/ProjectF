f22331306fa46e7b9cd238ba31416e8ff5b2699d
lakhesis7 <lakhesis7@users.noreply.github.com>
2016-12-13 18:03:10 +1100

    Comment renderer with plugin-type rules

--- testing/renderer.py
+++ testing/renderer.py
@@ -3,38 +3,6 @@ import json
 from collections import OrderedDict
 from utils.time_this import TimeThis
 
-class MissingPattern(Exception): pass
-
-class BarkBrown:
-    def __init__(self, rules):
-        self.mapping = OrderedDict()
-        for rule in rules: self.mapping[rule.tag] = rule
-
-        for rule in self.mapping.values():
-            rule.rules = tuple(self.mapping[r] for r in rule.rules)
-            rule.parse = self.parse
-
-    def parse(self, text, rules=None):
-        if rules is None: rules = self.mapping.values()
-
-        repls = ['{}'] * text.count('{}')  # Maintain any '{}' within the input text
-
-        for rule in rules:
-            while True:
-                m = rule.regex.search(text)
-                if m is None: break
-                preceeding_s = text.count('{}', 0, m.start())
-                within_s = m.group().count('{}')
-
-                # Replacement's for {}'s within the match's text are replaced with the transform text full fleshed out
-                repls[preceeding_s: preceeding_s + within_s] = [
-                    rule.transform(m).format(*repls[preceeding_s: preceeding_s + within_s])
-                ]
-
-                text = text[:m.start()] + '{}' + text[m.end():]
-
-        return text.format(*repls)
-
 class Rule:
     tag = None
     regex = None
@@ -61,7 +29,6 @@ class Link(Rule):
     regex = re.compile(r'\[\[(\S+)( (\S+))?\]\]')
 
     def transform(self, match): return match.group()
-
 
 class UserMention(Rule):
     tag = 'user_mention'
@@ -95,3 +62,42 @@ class Subscript(Rule):
     rules = ('bold_italics', 'subscript')
 
     def transform(self, match): return '<sub>{}</sub>'.format(self.parse(match.group(1), self.rules))
+
+DEFAULT_RULES = (
+    EscapedChar(),
+    CodeBlock(),
+    UserMention(),
+    Emoji(),
+    BoldItalics(),
+    Subscript()
+)
+
+class BarkBrown:
+    def __init__(self, rules=DEFAULT_RULES):
+        self.mapping = OrderedDict()
+        for rule in rules: self.mapping[rule.tag] = rule
+
+        for rule in self.mapping.values():
+            rule.rules = tuple(self.mapping[r] for r in rule.rules)
+            rule.parse = self.parse
+
+    def parse(self, text, rules=None):
+        if rules is None: rules = self.mapping.values()
+
+        repls = ['{}'] * text.count('{}')  # Maintain any '{}' within the input text
+
+        for rule in rules:
+            while True:
+                m = rule.regex.search(text)
+                if m is None: break
+                repls_start_index = text.count('{}', 0, m.start())
+                repls_end_index = repls_start_index + m.group().count('{}')
+
+                # Replacement's for {}'s within the match's text are replaced with the transform text full fleshed out
+                repls[repls_start_index: repls_end_index] = [
+                    rule.transform(m).format(*repls[repls_start_index: repls_end_index])
+                ]
+
+                text = text[:m.start()] + '{}' + text[m.end():]
+
+        return text.format(*repls)
