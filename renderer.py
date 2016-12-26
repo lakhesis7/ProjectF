7ee7f7e4e042dee7ed4c1cfba3968cb486ca1c7a
lakhesis7 <lakhesis7@users.noreply.github.com>
2016-12-26 22:42:22 +1100

    Improved unittests

--- renderer.py
+++ renderer.py
@@ -1,14 +1,17 @@
-import json
 import re
+from itertools import zip_longest
+from operator import itemgetter
+from emojis import EMOJIs as _EMOJIs
 
 # TODO: Implement common rule patterns as customizable classes
 class RegexRule:
-    def __init__(self, name, pattern, descent_rules=None, func=None, is_default=True, flags=re.DOTALL | re.MULTILINE):
+    def __init__(self, name, pattern, descent_rules=None, transform_func=None, is_default=True,
+                 flags=re.DOTALL | re.MULTILINE):
         self.name = name
         self.pattern = pattern
         self.regex = re.compile(pattern, flags=flags)
         self.descent_rules = descent_rules or ()
-        self.func = func
+        self.transform_func = transform_func
         self.is_default = is_default
 
     def __repr__(self): return f'{self.__class__.__name__}({self.name!r}, {self.pattern!r})'
@@ -26,7 +29,7 @@ class Renderer:
                 self.regexes[p.name] = re.compile(
                     '|'.join(self.patterns[r].pattern for r in p.descent_rules), flags=re.DOTALL | re.MULTILINE
                 )
-            if not p.func: p.func = getattr(self, f'_parse_{p.name}', self._parse_DEFAULT)
+            if not p.transform_func: p.transform_func = getattr(self, f'transform_{p.name}', self.transform_DEFAULT)
 
         self.preprocessors = []
         self.postprocessors = []
@@ -36,6 +39,7 @@ class Renderer:
             elif key.startswith('post_'): self.postprocessors.append(getattr(self, key))
 
     def parse(self, text):
+        if not isinstance(text, str): raise TypeError(f'expected string object, not {text.__class__.__name__}')
         for p in self.preprocessors: text = p(text)
         text = self._parse(text)
         for p in self.postprocessors: text = p(text)
@@ -48,7 +52,7 @@ class Renderer:
             match = self.regexes[descent_rule].search(text)
             if not match: break
             try:
-                output += text[:match.start()] + self.patterns[match.lastgroup].func(match)
+                output += text[:match.start()] + self.patterns[match.lastgroup].transform_func(match)
                 text = text[match.end():]
             except InvalidMatch:
                 output += text[0]
@@ -56,16 +60,16 @@ class Renderer:
 
         return output + text
 
-    def _parse_DEFAULT(self, match):
+    def transform_DEFAULT(self, match):
         return match[0]
 
 class DefaultRenderer(Renderer):
     def __init__(self):
         super().__init__([
-            RegexRule('EDGE_WHITESPACE', r'(?P<EDGE_WHITESPACE>(?-m:^\s+|\s+$))'),
-            RegexRule('NEWLINE', r'(?P<NEWLINE>\r\n|\r)'),
-            RegexRule('CODE', r'(?P<CODE>(?<!\\){{3}(?P<CODE_TEXT>[^{}].*?)(?<!\\)}{3})'),
-            RegexRule('ESCAPE', r'(?P<ESCAPE>\\(?P<ESCAPE_TEXT>[-\\*\[\]_^=\n{}]))'),
+            RegexRule('WHITESPACE', r'(?P<WHITESPACE>^\s+|\s+$)', is_default=False),
+            RegexRule('NEWLINE', r'(?P<NEWLINE>\r\n|\r)', is_default=False),
+            RegexRule('CODE', r'(?P<CODE>(?<!\\){{3}(?P<CODE_TEXT>.*?)(?<!\\)}{3})', is_default=False),
+            RegexRule('ESCAPE', r'(?P<ESCAPE>\\(?P<ESCAPE_TEXT>[-\\*\[\]_^=\n{}]))', is_default=False),
             RegexRule(
                 'LINK',
                 r'(?P<LINK>'
@@ -80,74 +84,68 @@ class DefaultRenderer(Renderer):
             RegexRule('EMOJI', r'(?P<EMOJI>:(?P<EMOJI_TEXT>[-+a-zA-Z0-9_]+?):)'),
             RegexRule(
                 'TABLE',
-                r'(?P<TABLE>^(?P<TABLE_FIRSTROW>\|(.*?\|)+?$)(?P<TABLE_OTHERROWS>(\n^\|(.*?\|)+$)+)?)',
+                r'(?P<TABLE>(?-s:^\|(.*?(?<!\\)\|)+(\n\|(.*?(?<!\\)\|)+$)*($|\n)))',
                 ('ESCAPE', 'CODE', 'LINK', 'USER_MENTION', 'EMOJI', 'HEADING', 'BULLET_LIST', 'NUMBER_LIST',
                  'BOLD', 'ITALICS', 'UNDERLINE', 'STRIKETHROUGH', 'SUPERSCRIPT', 'SUBSCRIPT')),
             RegexRule(
                 'BULLET_LIST',
-                r'(?P<BULLET_LIST>^\* (?P<BL_FIRSTROW>.*)(?P<BL_OTHERROWS>(\n\*+ .*)*))',
+                r'(?P<BULLET_LIST>^\* .*?$((?<!\\)\n\*+ .*?$)*)',
                 ('ESCAPE', 'CODE', 'LINK', 'USER_MENTION', 'EMOJI', 'HEADING', 'BOLD', 'ITALICS', 'UNDERLINE',
                  'STRIKETHROUGH', 'SUPERSCRIPT', 'SUBSCRIPT')),
             RegexRule(
                 'NUMBER_LIST',
-                r'(?P<NUMBER_LIST>^# (?P<NL_FIRSTROW>.*)(?P<NL_OTHERROWS>(\n#+ .*)*))',
+                r'(?P<NUMBER_LIST>^# .*?$((?<!\\)\n#+ .*?$)*)',
                 ('ESCAPE', 'CODE', 'LINK', 'USER_MENTION', 'EMOJI', 'HEADING', 'BOLD', 'ITALICS', 'UNDERLINE',
                  'STRIKETHROUGH', 'SUPERSCRIPT', 'SUBSCRIPT')),
             RegexRule(
                 'HEADING',
-                r'(?P<HEADING>^(?:(?P<HEADING_LEVEL>=+) )(?P<HEADING_TEXT>.+?$))',
+                r'(?P<HEADING>^(?P<HEADING_LEVEL>=+) (?P<HEADING_TEXT>.+?$))',
                 ('ESCAPE', 'LINK', 'USER_MENTION', 'EMOJI', 'BOLD', 'ITALICS', 'UNDERLINE', 'STRIKETHROUGH',
                  'SUPERSCRIPT',
                  'SUBSCRIPT')),
+            RegexRule('HORIZ_RULE', r'(?P<HORIZ_RULE>^-{4,}$)'),
             RegexRule(
                 'BOLD',
-                r'(?P<BOLD>(?<!\\)\*{2}(?P<BOLD_TEXT>[^*].*?)(?<!\\)\*{2})',
-                ('ESCAPE', 'ITALICS', 'UNDERLINE', 'STRIKETHROUGH', 'SUPERSCRIPT', 'SUBSCRIPT')),
+                r'(?P<BOLD>(?<!\\)\*{2}(?P<BOLD_TEXT>.*?)(?<!\\)\*{2})',
+                ('ESCAPE', 'EMOJI', 'ITALICS', 'UNDERLINE', 'STRIKETHROUGH', 'SUPERSCRIPT', 'SUBSCRIPT')),
             RegexRule(
                 'ITALICS',
-                r'(?P<ITALICS>(?<!\\)/{2}(?P<ITALICS_TEXT>[^/].*?)(?<!\\)/{2})',
-                ('ESCAPE', 'BOLD', 'UNDERLINE', 'STRIKETHROUGH', 'SUPERSCRIPT', 'SUBSCRIPT')),
+                r'(?P<ITALICS>(?<!\\)/{2}(?P<ITALICS_TEXT>.*?)(?<!\\)/{2})',
+                ('ESCAPE', 'EMOJI', 'BOLD', 'UNDERLINE', 'STRIKETHROUGH', 'SUPERSCRIPT', 'SUBSCRIPT')),
             RegexRule(
                 'UNDERLINE',
-                r'(?P<UNDERLINE>(?<!\\)_{2}(?P<UNDERLINE_TEXT>[^_].*?)(?<!\\)_{2})',
-                ('ESCAPE', 'BOLD', 'ITALICS', 'STRIKETHROUGH', 'SUPERSCRIPT', 'SUBSCRIPT',)),
+                r'(?P<UNDERLINE>(?<!\\)_{2}(?P<UNDERLINE_TEXT>.*?)(?<!\\)_{2})',
+                ('ESCAPE', 'EMOJI', 'BOLD', 'ITALICS', 'STRIKETHROUGH', 'SUPERSCRIPT', 'SUBSCRIPT',)),
             RegexRule(
                 'STRIKETHROUGH',
-                r'(?P<STRIKETHROUGH>(?<!\\)-{2}(?P<STRIKETHROUGH_TEXT>[^-].*?)(?<!\\)-{2})',
-                ('ESCAPE', 'BOLD', 'ITALICS', 'UNDERLINE', 'SUPERSCRIPT', 'SUBSCRIPT')),
+                r'(?P<STRIKETHROUGH>(?<!\\)-{2}(?P<STRIKETHROUGH_TEXT>.*?)(?<!\\)-{2})',
+                ('ESCAPE', 'EMOJI', 'BOLD', 'ITALICS', 'UNDERLINE', 'SUPERSCRIPT', 'SUBSCRIPT')),
             RegexRule(
                 'SUPERSCRIPT',
-                r'(?P<SUPERSCRIPT>(?<!\\)\^{2}(?P<SUPERSCRIPT_TEXT>[^^].*?)(?<!\\)\^{2})',
-                ('ESCAPE', 'BOLD', 'ITALICS', 'UNDERLINE', 'STRIKETHROUGH')),
+                r'(?P<SUPERSCRIPT>(?<!\\)\^{2}(?P<SUPERSCRIPT_TEXT>.*?)(?<!\\)\^{2})',
+                ('ESCAPE', 'EMOJI', 'BOLD', 'ITALICS', 'UNDERLINE', 'STRIKETHROUGH')),
             RegexRule(
                 'SUBSCRIPT',
-                r'(?P<SUBSCRIPT>(?<!\\)~{2}(?P<SUBSCRIPT_TEXT>[^~].*?)(?<!\\)~{2})',
-                ('ESCAPE', 'BOLD', 'ITALICS', 'UNDERLINE', 'STRIKETHROUGH')),
-            RegexRule('HORIZ_RULE', r'(?P<HORIZ_RULE>^-{4,}$)'),
+                r'(?P<SUBSCRIPT>(?<!\\)~{2}(?P<SUBSCRIPT_TEXT>.*?)(?<!\\)~{2})',
+                ('ESCAPE', 'EMOJI', 'BOLD', 'ITALICS', 'UNDERLINE', 'STRIKETHROUGH')),
         ])
 
-        self.emojis = {}
-        with open('eac.json', 'rt') as f:
-            for key, value in json.load(f).items():
-                if '-' in key: key = ''.join(chr(int(x, base=16)) for x in key.split('-'))
-                else: key = chr(int(key, base=16))
-                self.emojis[value['alpha_code']] = key
-                if value['aliases']:
-                    for alias in value['aliases'].split('|'):
-                        self.emojis[alias] = key
+    def pre_WHITESPACE(self, text):
+        return self.patterns['WHITESPACE'].regex.sub('', text)
 
-    def _parse_EDGE_WHITESPACE(self, match):
-        return ''
+    def pre_NEWLINES(self, text):
+        return self.patterns['NEWLINE'].regex.sub('\n', text)
 
-    def _parse_NEWLINE(self, match): return '\n'
+    # def transform_NEWLINE(self, match): return '\n'
 
-    def _parse_CODE(self, match):
+    def transform_CODE(self, match):
+        if not match['CODE_TEXT']: return ''
         return '<code>{}</code>'.format(match['CODE_TEXT'])
 
-    def _parse_ESCAPE(self, match):
+    def transform_ESCAPE(self, match):
         return match['ESCAPE_TEXT']
 
-    def _parse_LINK(self, match):
+    def transform_LINK(self, match):
         result = 'LINKTO:{}'.format(match['URL'])
         if match['DESCRIPTION']: result += ':{}'.format(self._parse(match['DESCRIPTION'], 'LINK'))
         if match['EXTENSIONS']:
@@ -156,44 +154,90 @@ class DefaultRenderer(Renderer):
                 if k in extensions: result += v
         return result
 
-    def _parse_USER_MENTION(self, match):
+    def transform_USER_MENTION(self, match):
         return 'LINKTOUSER:{}'.format(match['USERNAME'])
 
-    def _parse_EMOJI(self, match):
-        try: return self.emojis[match[0].lower()]
+    def transform_EMOJI(self, match):
+        try: return _EMOJIs[match[0].lower()]
         except KeyError: raise InvalidMatch(match[0])
 
-    def _parse_TABLE(self, match):
-        return match[0]
+    def transform_TABLE(self, match):
+        rows = re.split(r'(?<=(?<!\\)\|)\n', match[0])
+        output = '<table>'
+        for row in rows:
+            output += '<tr>'
+            for cell in re.split(r'(?<!\\)\|', row)[1:-1]:
+                output += f'<td>{cell}</td>'
+            output += '</tr>'
+        return output + '</table>'
 
-    def _parse_BULLET_LIST(self, match):
-        return match[0]
+    def transform_BULLET_LIST(self, match):
+        rows = [(len(m[0]), self._parse(m[1], 'BULLET_LIST')) for m in re.findall(r'(\*+) (.*?)($|(?<!\\)\n)', match[0])]
+        current_level = 0
+        output = ''
 
-    def _parse_NUMBER_LIST(self, match):
-        return match[0]
+        for row in rows:
+            if row[0] > current_level:
+                if row[0] - current_level > 1: raise InvalidMatch(match[0])
+                output += '<ul><li>' + row[1]
+            elif row[0] < current_level:
+                output += '</li></ul>' * (current_level - row[0]) + '<li>' + row[1]
+            else: output += '</li><li>' + row[1]
+            current_level = row[0]
+        return output + '</li></ul>' * current_level
 
-    def _parse_HEADING(self, match):
-        return match[0]
+    def transform_NUMBER_LIST(self, match):
+        rows = [(len(m[0]), self._parse(m[1], 'NUMBER_LIST')) for m in re.findall(r'(#+) (.*?)($|(?<!\\)\n)', match[0])]
+        types = ['1', 'a', 'i']
+        current_level = 0
+        output = ''
 
-    def _parse_BOLD(self, match):
+        for row in rows:
+            if row[0] > current_level:
+                if row[0] - current_level > 1: raise InvalidMatch(match[0])
+                output += f'<ol type="{types[current_level % len(types)]}"><li>' + row[1]
+            elif row[0] < current_level:
+                output += '</li></ol>' * (current_level - row[0]) + '<li>' + row[1]
+            else: output += '</li><li>' + row[1]
+            current_level = row[0]
+        return output + '</li></ol>' * current_level
+
+    def transform_HEADING(self, match):
+        level = min(len(match["HEADING_LEVEL"]), 6)
+        return f'<h{level}>{match["HEADING_TEXT"]}</h{level}>'
+
+    def transform_BOLD(self, match):
+        if not match['BOLD_TEXT']: return ''
         return '<b>{}</b>'.format(self._parse(match['BOLD_TEXT'], 'BOLD'))
 
-    def _parse_ITALICS(self, match):
+    def transform_ITALICS(self, match):
+        if not match['ITALICS_TEXT']: return ''
         return '<i>{}</i>'.format(self._parse(match['ITALICS_TEXT'], 'ITALICS'))
 
-    def _parse_UNDERLINE(self, match):
+    def transform_UNDERLINE(self, match):
+        if not match['UNDERLINE_TEXT']: return ''
         return '<u>{}</u>'.format(self._parse(match['UNDERLINE_TEXT'], 'UNDERLINE'))
 
-    def _parse_STRIKETHROUGH(self, match):
-        return '<str>{}</str>'.format(self._parse(match['STRIKETHROUGH_TEXT'], 'STRIKETHROUGH'))
+    def transform_STRIKETHROUGH(self, match):
+        if not match['STRIKETHROUGH_TEXT']: return ''
+        return '<s>{}</s>'.format(self._parse(match['STRIKETHROUGH_TEXT'], 'STRIKETHROUGH'))
 
-    def _parse_SUPERSCRIPT(self, match):
+    def transform_SUPERSCRIPT(self, match):
+        if not match['SUPERSCRIPT_TEXT']: return ''
         return '<sup>{}</sup>'.format(self._parse(match['SUPERSCRIPT_TEXT'], 'SUPERSCRIPT'))
 
-    def _parse_SUBSCRIPT(self, match):
+    def transform_SUBSCRIPT(self, match):
+        if not match['SUBSCRIPT_TEXT']: return ''
         return '<sub>{}</sub>'.format(self._parse(match['SUBSCRIPT_TEXT'], 'SUBSCRIPT'))
 
-    def _parse_HORIZ_RULE(self, match):
-        return '|' * 10
+    def transform_HORIZ_RULE(self, match):
+        return '<hr>'
+
+# FIXME: **some{{code**text}}
 
 # TODO: """(?P<URL_VALIDATOR>[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[A-Za-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?)"""
+
+with open('result.html', 'wt', encoding='utf-8') as f:
+    f.write('<html><body>')
+    f.write(DefaultRenderer().parse(''))
+    f.write('</body></html>')
