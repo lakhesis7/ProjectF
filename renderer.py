16e027432fa6bc4a8133d5a36425262bd1f21656
lakhesis7 <lakhesis7@users.noreply.github.com>
2016-12-27 11:30:41 +1100

    Added removal of non-printable characters (0x00-0x1F + 0x7F-0x9F)

--- renderer.py
+++ renderer.py
@@ -1,7 +1,6 @@
 import re
-from itertools import zip_longest
-from operator import itemgetter
 from emojis import EMOJIs as _EMOJIs
+from itertools import chain
 
 # TODO: Implement common rule patterns as customizable classes
 class RegexRule:
@@ -70,35 +69,35 @@ class DefaultRenderer(Renderer):
             RegexRule('NEWLINE', r'(?P<NEWLINE>\r\n|\r)', is_default=False),
             RegexRule(
                 'ESCAPE',
-                r'(?P<ESCAPE>(?<!\\)\\(?P<SINGLE_ESCAPE>.)|(?<!\\){{{(?P<BLOCK_ESCAPE>.+?)}}})',
+                r'(?<!\\)(?P<ESCAPE>\\(?P<SINGLE_ESCAPE>.)|{{{(?P<BLOCK_ESCAPE>.+?)(?<!\\)}}})',
                 is_default=False),
             RegexRule(
                 'LINK',
-                r'(?P<LINK>'
-                r'(?<!\\)\[\['
-                r'(?P<URL>\S+?) *?'
-                r'(\|(?P<DESCRIPTION>.*?)(?P<EXTENSIONS>((?<!\\)\|.*?)*))?'
-                r'(?<!\\)\]\])',
+                '(?P<LINK>\[\[(?P<URL>\S+?) *?(\|(?P<DESCRIPTION>.*?)(?P<EXTENSIONS>(\|.*?)*))?\]\])',
                 ('ESCAPE', 'EMOJI', 'BOLD', 'ITALICS', 'UNDERLINE', 'STRIKETHROUGH', 'SUPERSCRIPT', 'SUBSCRIPT')),
             RegexRule(
                 'USER_MENTION',
-                r'(?P<USER_MENTION>(?<!\\)@(?P<USERNAME>[a-zA-Z0-9][a-zA-Z0-9_]{2,23}[a-zA-Z0-9]))'),
+                r'(?P<USER_MENTION>@(?P<USERNAME>[a-zA-Z0-9][a-zA-Z0-9_]{2,23}[a-zA-Z0-9]))'),
             RegexRule('EMOJI', r'(?P<EMOJI>:(?P<EMOJI_TEXT>[-+a-zA-Z0-9_]+?):)'),
             RegexRule(
                 'TABLE',
-                r'(?P<TABLE>(?-s:^\|(.*?(?<!\\)\|)+(\n\|(.*?(?<!\\)\|)+$)*($|\n)))',
+                r'(?P<TABLE>(?-s:^\|(.*?\|)+(\n\|(.*?\|)+$)*($|\n)))',
                 ('ESCAPE', 'LINK', 'USER_MENTION', 'EMOJI', 'HEADING', 'BULLET_LIST', 'NUMBER_LIST',
                  'BOLD', 'ITALICS', 'UNDERLINE', 'STRIKETHROUGH', 'SUPERSCRIPT', 'SUBSCRIPT')),
+            RegexRule('TABLE_ROW_SPLIT', r'(?<=\|)\n', is_default=False),
+            RegexRule('TABLE_CELL_SPLIT', r'\|', is_default=False),
             RegexRule(
                 'BULLET_LIST',
-                r'(?P<BULLET_LIST>^\* .*?$((?<!\\)\n\*+ .*?$)*)',
+                r'(?P<BULLET_LIST>^\* .*?$(\n\*+ .*?$)*)',
                 ('ESCAPE', 'LINK', 'USER_MENTION', 'EMOJI', 'HEADING', 'BOLD', 'ITALICS', 'UNDERLINE',
                  'STRIKETHROUGH', 'SUPERSCRIPT', 'SUBSCRIPT')),
+            RegexRule('BULLET_LIST_SPLIT', r'(?P<BULLET_LIST_SPLIT>(\*+) (.*?)($|\n))', is_default=False),
             RegexRule(
                 'NUMBER_LIST',
-                r'(?P<NUMBER_LIST>^# .*?$((?<!\\)\n#+ .*?$)*)',
+                r'(?P<NUMBER_LIST>^# .*?$(\n#+ .*?$)*)',
                 ('ESCAPE', 'LINK', 'USER_MENTION', 'EMOJI', 'HEADING', 'BOLD', 'ITALICS', 'UNDERLINE',
                  'STRIKETHROUGH', 'SUPERSCRIPT', 'SUBSCRIPT')),
+            RegexRule('NUMBER_LIST_SPLIT', r'(?P<NUMBER_LIST_SPLIT>(#+) (.*?)($|\n))', is_default=False),
             RegexRule(
                 'HEADING',
                 r'(?P<HEADING>^(?P<HEADING_LEVEL>=+) (?P<HEADING_TEXT>.+?$))',
@@ -108,29 +107,33 @@ class DefaultRenderer(Renderer):
             RegexRule('HORIZ_RULE', r'(?P<HORIZ_RULE>^-{4,}$)'),
             RegexRule(
                 'BOLD',
-                r'(?P<BOLD>(?<!\\)\*{2}(?P<BOLD_TEXT>.*?)(?<!\\)\*{2})',
+                r'(?P<BOLD>\*{2}(?P<BOLD_TEXT>.*?)\*{2})',
                 ('ESCAPE', 'EMOJI', 'ITALICS', 'UNDERLINE', 'STRIKETHROUGH', 'SUPERSCRIPT', 'SUBSCRIPT')),
             RegexRule(
                 'ITALICS',
-                r'(?P<ITALICS>(?<!\\)/{2}(?P<ITALICS_TEXT>.*?)(?<!\\)/{2})',
+                r'(?P<ITALICS>/{2}(?P<ITALICS_TEXT>.*?)/{2})',
                 ('ESCAPE', 'EMOJI', 'BOLD', 'UNDERLINE', 'STRIKETHROUGH', 'SUPERSCRIPT', 'SUBSCRIPT')),
             RegexRule(
                 'UNDERLINE',
-                r'(?P<UNDERLINE>(?<!\\)_{2}(?P<UNDERLINE_TEXT>.*?)(?<!\\)_{2})',
+                r'(?P<UNDERLINE>_{2}(?P<UNDERLINE_TEXT>.*?)_{2})',
                 ('ESCAPE', 'EMOJI', 'BOLD', 'ITALICS', 'STRIKETHROUGH', 'SUPERSCRIPT', 'SUBSCRIPT',)),
             RegexRule(
                 'STRIKETHROUGH',
-                r'(?P<STRIKETHROUGH>(?<!\\)-{2}(?P<STRIKETHROUGH_TEXT>.*?)(?<!\\)-{2})',
+                r'(?P<STRIKETHROUGH>-{2}(?P<STRIKETHROUGH_TEXT>.*?)-{2})',
                 ('ESCAPE', 'EMOJI', 'BOLD', 'ITALICS', 'UNDERLINE', 'SUPERSCRIPT', 'SUBSCRIPT')),
             RegexRule(
                 'SUPERSCRIPT',
-                r'(?P<SUPERSCRIPT>(?<!\\)\^{2}(?P<SUPERSCRIPT_TEXT>.*?)(?<!\\)\^{2})',
+                r'(?P<SUPERSCRIPT>\^{2}(?P<SUPERSCRIPT_TEXT>.*?)\^{2})',
                 ('ESCAPE', 'EMOJI', 'BOLD', 'ITALICS', 'UNDERLINE', 'STRIKETHROUGH')),
             RegexRule(
                 'SUBSCRIPT',
-                r'(?P<SUBSCRIPT>(?<!\\)~{2}(?P<SUBSCRIPT_TEXT>.*?)(?<!\\)~{2})',
+                r'(?P<SUBSCRIPT>~{2}(?P<SUBSCRIPT_TEXT>.*?)~{2})',
                 ('ESCAPE', 'EMOJI', 'BOLD', 'ITALICS', 'UNDERLINE', 'STRIKETHROUGH')),
         ])
+
+    nonprintable_table = {c: None for c in chain(range(0, 32), range(127, 160))}
+    def pre_NONPRINTABLE(self, text):
+        return text.translate(self.nonprintable_table)
 
     def pre_WHITESPACE(self, text):
         return self.patterns['WHITESPACE'].regex.sub('', text)
@@ -142,21 +145,14 @@ class DefaultRenderer(Renderer):
 
     def post_ESCAPE(self, text):
         for m in self.escape_groups:
-            if m['SINGLE_ESCAPE']: text = text.replace('\x00', m['SINGLE_ESCAPE'], 1)
-            else: text = text.replace('\x00', f"<code>{m['BLOCK_ESCAPE']}</code>", 1)
-        self.escape_groups = []
+            text = text.replace('\x00', m['SINGLE_ESCAPE'] or m['BLOCK_ESCAPE'], 1)
         return text
 
     def pre_NEWLINES(self, text):
         return self.patterns['NEWLINE'].regex.sub('\n', text)
 
-    # def transform_NEWLINE(self, match): return '\n'
-
-    # def transform_CODE(self, match):
-    #     if not match['CODE_TEXT']: return ''
-    #     return '<code>{}</code>'.format(match['CODE_TEXT'])
-
     def transform_LINK(self, match):
+        if '\x00' in match['URL']: raise InvalidMatch(match[0])
         result = 'LINKTO:{}'.format(match['URL'])
         if match['DESCRIPTION']: result += ':{}'.format(self._parse(match['DESCRIPTION'], 'LINK'))
         if match['EXTENSIONS']:
@@ -173,17 +169,18 @@ class DefaultRenderer(Renderer):
         except KeyError: raise InvalidMatch(match[0])
 
     def transform_TABLE(self, match):
-        rows = re.split(r'(?<=(?<!\\)\|)\n', match[0])
+        rows = self.patterns['TABLE_ROW_SPLIT'].regex.split(match[0])
         output = '<table>'
         for row in rows:
             output += '<tr>'
-            for cell in re.split(r'(?<!\\)\|', row)[1:-1]:
+            for cell in self.patterns['TABLE_CELL_SPLIT'].regex.split(row)[1:-1]:
                 output += f'<td>{cell}</td>'
             output += '</tr>'
         return output + '</table>'
 
     def transform_BULLET_LIST(self, match):
-        rows = [(len(m[0]), self._parse(m[1], 'BULLET_LIST')) for m in re.findall(r'(\*+) (.*?)($|(?<!\\)\n)', match[0])]
+        rows = [(len(m[0]), self._parse(m[1], 'BULLET_LIST'))
+                for m in self.patterns['BULLET_LIST_SPLIT'].regex.findall(match[0])]
         current_level = 0
         output = ''
 
@@ -198,7 +195,8 @@ class DefaultRenderer(Renderer):
         return output + '</li></ul>' * current_level
 
     def transform_NUMBER_LIST(self, match):
-        rows = [(len(m[0]), self._parse(m[1], 'NUMBER_LIST')) for m in re.findall(r'(#+) (.*?)($|(?<!\\)\n)', match[0])]
+        rows = [(len(m[0]), self._parse(m[1], 'NUMBER_LIST'))
+                for m in self.patterns['NUMBER_LIST_SPLIT'].regex.findall(match[0])]
         types = ['1', 'a', 'i']
         current_level = 0
         output = ''
