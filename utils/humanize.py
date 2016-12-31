70a92a8f7ee5cbf3ac249f7fa1bb15513a62910b
lakhesis7 <lakhesis7@users.noreply.github.com>
2017-01-01 10:32:54 +1100

    Updated humanize module to 3.6. Now using more efficient algorithm at the cost of less customization

--- utils/humanize.py
+++ utils/humanize.py
@@ -1,29 +1,22 @@
 from datetime import datetime, timedelta
 
-_INTERVALS = (
-    ('year',   timedelta(weeks=52),     timedelta(weeks=52)),
-    ('month',  timedelta(days=30.4375), timedelta(days=30.4375)),
-    ('week',   timedelta(weeks=1),      timedelta(weeks=1)),
-    ('day',    timedelta(days=1),       timedelta(days=1)),
-    ('hour',   timedelta(hours=1),      timedelta(hours=1)),
-    ('minute', timedelta(minutes=1),    timedelta(minutes=1)),
-    ('second', timedelta(seconds=1),    timedelta(seconds=0)),
-)
-_ZERO_TIMEDELTA = timedelta(0)
+INTERVALS = {
+    timedelta(weeks=52)    : 'year',
+    timedelta(days=30.4375): 'month',
+    timedelta(weeks=1)     : 'week',
+    timedelta(days=1)      : 'day',
+    timedelta(hours=1)     : 'hour',
+    timedelta(minutes=1)   : 'minute',
+    timedelta(seconds=1)   : 'second',
+}
 
 def convert_timedelta(td):
-    is_future = td < _ZERO_TIMEDELTA
-    td = abs(td)
-
-    for name, unit, minimum in _INTERVALS:
-        if td >= minimum:
-            difference = td / unit
-            return '%0.1f %s%s %s' % (
-                difference,
-                name,
-                's' if difference != 1.0 else '',
-                'to go' if is_future else 'ago'
-            )
+    for unit, unit_name in INTERVALS.items():
+        if unit <= td:
+            result = td / unit
+            if result != 1.0: unit_name += 's'
+            return f'{result:.2g} {unit_name} ago'
+    return 'just now'
 
 def convert_datetime(dt, now=None):
     if now is None: now = datetime.utcnow()
