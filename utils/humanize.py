84775dd67fd507eba4999cd630b3a5f2333e93d0
lakhesis7 <lakhesis7@users.noreply.github.com>
2017-01-01 11:54:19 +1100

    Better method names

--- utils/humanize.py
+++ utils/humanize.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 
-INTERVALS = {
+TIMEDELTA_INTERVALS = {
     timedelta(weeks=52)    : 'year',
     timedelta(days=30.4375): 'month',
     timedelta(weeks=1)     : 'week',
@@ -10,14 +10,14 @@ INTERVALS = {
     timedelta(seconds=1)   : 'second',
 }
 
-def convert_timedelta(td):
-    for unit, unit_name in INTERVALS.items():
+def humanize_timedelta(td):
+    for unit, unit_name in TIMEDELTA_INTERVALS.items():
         if unit <= td:
             result = td / unit
             if result != 1.0: unit_name += 's'
             return f'{result:.2g} {unit_name} ago'
     return 'just now'
 
-def convert_datetime(dt, now=None):
+def humanize_datetime(dt, now=None):
     if now is None: now = datetime.utcnow()
-    return convert_timedelta(now - dt)
+    return humanize_timedelta(now - dt)
